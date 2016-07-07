5a9dc91b5271c908002bce05a267d66cab037a52
Boy van Amstel <boyvanamstel@Boys-MacBook-Pro.local>
2016-07-07 15:04:32 +0200

    Add GIF indicator

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -17,6 +17,22 @@ function bindButtons() {
       button = event.target;
       preview.setAttribute('src', button.dataset.animationSrc);
       button.remove();
+      // Hide gif indicator
+      var gifIndicators = document.querySelectorAll('.c-preview__gifindicator');
+      for (var j = 0; j < gifIndicators.length; j++) {
+        var gifIndicator = gifIndicators[j];
+        gifIndicator.remove();
+      }
+
+      // Track event
+      if (ga) {
+        ga('send', {
+          hitType: 'event',
+          eventCategory: 'Preview GIF',
+          eventAction: 'play',
+          eventLabel: 'Coming Soon Page'
+        });
+      }
     }, false );
   }
   // Hide modal
@@ -42,12 +58,30 @@ function bindButtons() {
         modalContainer.classList.add('is-visible');
       }
       downloadModal.classList.add('is-visible');
+
+      // Track event
+      if (ga) {
+        ga('send', {
+          hitType: 'event',
+          eventCategory: 'Download Modal',
+          eventAction: 'open',
+          eventLabel: 'Coming Soon Page'
+        });
+      }
     }, false);
   }
   // Mailinglist button
   var mailinglistButton = document.querySelector('.c-modal-download .c-mailinglist--submit .c-button--confirm');
   mailinglistButton.addEventListener('click', function(event) {
     hideModal();
+
+    // Track event
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Download Modal',
+      eventAction: 'submit',
+      eventLabel: 'Coming Soon Page'
+    });
   });
 }
 bindButtons();
